c604789922bbe6ee10999bfc5a38535b55ba809d
Hugo Calmels <calmels.hugo@yahoo.fr>
2026-03-14 22:06:26 +0100

    first commit

---
+++ Backend/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { RoomsModule } from './rooms/rooms.module';
+import { BookingsModule } from './bookings/bookings.module';
+
+@Module({
+  imports: [RoomsModule, BookingsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
